242d724511284afc1f08e8de8c44187bf5908ba8
huichen <1065521866@qq.com>
2025-11-16 23:13:11 +0800

    chore: update README.md

--- generate_readme.sh
+++ generate_readme.sh
@@ -62,7 +62,9 @@ find . -mindepth 2 -maxdepth 3 -type f -name README.md \
 
         problem_markdown="$pnum. [$title_text]$(echo "$title_link" | sed -nE 's/\[.+\](\(.*\))/\1/p')"
 
-        echo "| $idx | $problem_markdown | $difficulty | $status | [$path]($path) |"
+        # ★ 在这里追加固定字符串
+        echo "| $idx | $problem_markdown | $difficulty | $status | [$path](${path}/problems/problem.go/problems/problem.go) |"
+
         idx=$((idx + 1))
     done
 } > "$OUTPUT_FILE"
