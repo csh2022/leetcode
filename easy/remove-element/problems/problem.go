d67ca63ce8613aa11faf1816fec9f0b0623b4a60
huichen <1065521866@qq.com>
2025-11-16 22:01:07 +0800

    feat: resolve remove-element problem

--- easy/remove-element/problems/problem.go
+++ easy/remove-element/problems/problem.go
@@ -1,42 +1,40 @@
 package problems
 
-// brute force, time complexity: O(n^2), space complexity: O(1)
-func TwoSum1(nums []int, target int) []int {
-	for i, _ := range nums {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				if i > j {
-					return []int{j, i}
-				}
-				return []int{i, j}
+func RemoveElement(nums []int, val int) int {
+	i, j := 0, len(nums)-1
+	for i <= j {
+		if i == j {
+			if nums[i] == val {
+				j--
+			} else {
+				i++
 			}
+			continue
+		}
+
+		if nums[i] == val && nums[j] == val {
+			j--
+			continue
+		}
+
+		if nums[i] != val && nums[j] != val {
+			i++
+			continue
+		}
+
+		if nums[i] == val && nums[j] != val {
+			nums[i] = nums[j]
+			i++
+			j--
+			continue
+		}
+
+		if nums[i] != val && nums[j] == val {
+			i++
+			j--
+			continue
 		}
 	}
 
-	return nil
+	return i
 }
-
-// hash table, time complexity: O(n), space complexity: O(n)
-func TwoSum2(nums []int, target int) []int {
-	hashTable := make(map[int]int)
-	for i, x := range nums {
-		if j, ok := hashTable[target-x]; ok {
-			if i > j {
-				return []int{j, i}
-			}
-			return []int{i, j}
-		}
-		hashTable[x] = i
-	}
-	return nil
-}
-
-// type TwoSumFunc func([]int, int) []int
-
-// var TwoSum TwoSumFunc = TwoSum1
-
-// var TwoSum func([]int, int) []int = TwoSum1
-
-// var TwoSum = TwoSum1
-
-var TwoSum = TwoSum2
--- easy/remove-element/problems/problem_test.go
+++ easy/remove-element/problems/problem_test.go
@@ -1,42 +1,49 @@
 package problems
 
 import (
-	"algo/lib"
 	"testing"
 )
 
-func TestTwoSum(t *testing.T) {
+func twoSlicesEqual(a, b []int, cnt int) bool {
+	if cnt != len(b) {
+		return false
+	}
+	for i := 0; i < cnt; i++ {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestTwoRemoveElement(t *testing.T) {
 	tests := []struct {
 		name     string
 		nums     []int
-		target   int
+		val      int
 		expected []int
 	}{
 		{
 			name:     "test1",
-			nums:     []int{2, 7, 11, 15},
-			target:   9,
-			expected: []int{0, 1},
+			nums:     []int{3, 2, 2, 3},
+			val:      3,
+			expected: []int{2, 2},
 		},
 		{
 			name:     "test2",
-			nums:     []int{3, 2, 4},
-			target:   6,
-			expected: []int{1, 2},
-		},
-		{
-			name:     "test3",
-			nums:     []int{1, 10, 2, 5, 100},
-			target:   101,
-			expected: []int{0, 4},
+			nums:     []int{0, 1, 2, 2, 3, 0, 4, 2},
+			val:      2,
+			expected: []int{0, 1, 4, 0, 3},
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			result := TwoSum(test.nums, test.target)
-			if !lib.IsTwoIntSlicesEqual(result, test.expected) {
-				t.Errorf("TwoSum(%v, %d) = %d, expected %v", test.nums, test.target, result, test.expected)
+			oldNums := make([]int, len(test.nums))
+			copy(oldNums, test.nums)
+			cnt := RemoveElement(test.nums, test.val)
+			if !twoSlicesEqual(test.nums, test.expected, cnt) {
+				t.Errorf("RemoveElement(%v, %d) = %d, expected %v", oldNums, test.val, test.nums[:cnt], test.expected)
 			}
 		})
 	}
