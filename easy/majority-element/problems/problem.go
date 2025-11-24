0d03758a19900afe57856a913f10b6049c3c0e5e
huichen <1065521866@qq.com>
2025-11-25 01:04:36 +0800

    feat: update majority-element problem

--- easy/majority-element/problems/problem.go
+++ easy/majority-element/problems/problem.go
@@ -1,26 +1,60 @@
 package problems
 
-func MajorityElement(nums []int) int {
+// 解法一：哈希表
+// func MajorityElement(nums []int) int {
+// 	hashTable := make(map[int]int)
+// 	for _, num := range nums {
+// 		if j, ok := hashTable[num]; ok {
+// 			hashTable[num] = j + 1
+// 		} else {
+// 			hashTable[num] = 1
+// 		}
+// 	}
+//
+// 	var targetNum int
+// 	maxCount := 0
+// 	for k, v := range hashTable {
+// 		if v > maxCount {
+// 			targetNum = k
+// 			maxCount = v
+// 		}
+// 	}
+//
+// 	return targetNum
+// }
+
+// 更简洁的写法
+func MajorityElement1(nums []int) int {
 	hashTable := make(map[int]int)
+	half := len(nums) / 2
 	for _, num := range nums {
-		if j, ok := hashTable[num]; ok {
-			hashTable[num] = j + 1
-		} else {
-			hashTable[num] = 1
+		hashTable[num] += 1
+		if hashTable[num] > half {
+			return num
 		}
 	}
 
-	var targetNum int
-	maxCount := 0
-	for k, v := range hashTable {
-		if v > maxCount {
-			targetNum = k
-			maxCount = v
-		}
-	}
-
-	return targetNum
+	return -1
 }
 
-// 哈希表
-// 排序
+// 解法二：排序
+
+// 解法三：摩尔投票
+func MajorityElement3(nums []int) int {
+	var candidate int
+	score := 0
+	for _, num := range nums {
+		if score == 0 {
+			candidate = num
+			score = 1
+		} else if num == candidate {
+			score += 1
+		} else {
+			score -= 1
+		}
+	}
+
+	return candidate
+}
+
+var MajorityElement = MajorityElement3
--- easy/majority-element/problems/problem_test.go
+++ easy/majority-element/problems/problem_test.go
@@ -12,17 +12,17 @@ func TestMajorityElement(t *testing.T) {
 	}{
 		{
 			name:     "test1",
-			nums:     []int{1, 2, 2, 3},
+			nums:     []int{1, 2, 2, 2, 4},
 			expected: 2,
 		},
 		{
 			name:     "test2",
-			nums:     []int{1, 1, 2, 3},
+			nums:     []int{1, 1, 1, 3, 4},
 			expected: 1,
 		},
 		{
 			name:     "test3",
-			nums:     []int{8, 9, 9, 9},
+			nums:     []int{8, 9, 9, 9, 10},
 			expected: 9,
 		},
 	}
